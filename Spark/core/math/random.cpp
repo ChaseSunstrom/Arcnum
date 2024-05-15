08f8f09e6fa5f5a64fbd4be3ed99ae05275bdd7e
Chase Sunstrom <122314695+ChaseSunstrom@users.noreply.github.com>
2024-05-15 16:18:28 -0500

    Update random.cpp

--- Spark/core/math/random.cpp
+++ Spark/core/math/random.cpp
@@ -1,28 +1,26 @@
 #include "random.hpp"
+#include "../logging/log.hpp"
 
 namespace Spark {
 namespace math {
-//i64 is int
-//u32 is unsigned int
-//f64 is double
 
 u32 roll_dice(const u32 min, const u32 max) {
     // Check if the range is valid
     if (min > max) {
-        std::cerr << "Error: min must be less than or equal to max" << std::endl;
+        SPARK_ERROR("Error: min must be less than or equal to max");
         return -1;
     }
 
     // Check if min or max is negative
     if (min < 0 || max < 0) {
-        std::cerr << "Error: min and max must be non-negative integers" << std::endl;
+        SPARK_ERROR("Error: min and max must be non-negative integers");
         return 0; // Return 0 to indicate error
     }
 
     // Create a random number generator and seed it using random_device
     static std::random_device rd;
     static std::mt19937 gen(rd());
-    std::uniform_int_distribution<unsigned int> dist(min, max);
+    std::uniform_int_distribution<u32> dist(min, max);
     
     // Generate a random number within the defined range (min to max)
     return static_cast<u32>(dist(gen));
@@ -33,7 +31,7 @@ std::vector<u32> multi_dice_roll(const u32 min, const u32 max, const u32 num_dic
 
     // Check if the range is valid
     if (min > max) {
-        std::cerr << "Error: min must be less than or equal to max" << std::endl;
+         SPARK_ERROR("Error: min must be less than or equal to max");
         return rolled_dice; //Return an empty array
     }
 
@@ -45,34 +43,34 @@ std::vector<u32> multi_dice_roll(const u32 min, const u32 max, const u32 num_dic
 }
 
 std::vector<u32> roll_unfair_dice(const u32 min, const unsigned int max, const std::vector<f64>& percentages) {
-    std::vector<u32> rolledDice;
+    std::vector<u32> rolled_dice;
 
     // Check if the range is valid
     if (min > max) {
-        std::cerr << "Error: min must be less than or equal to max" << std::endl;
-        return rolledDice;
+        SPARK_ERROR("Error: min must be less than or equal to max");
+        return rolled_dice;
     }
 
     // Check if the number of percentages matches the range
     if (percentages.size() != (max - min + 1)) {
         std::cerr << "Error: Number of percentages must match the range of values between min and max" << std::endl;
-        return rolledDice; // Return an empty vector
+        return rolled_dice; // Return an empty vector
     }
 
-    double sum = 0.0;
+    f64 sum = 0.0;
     // Check if any percentage is less than 0 and sum then up
-    for (double percentage : percentages) {
+    for (f64 percentage : percentages) {
         if (percentage < 0) {
-            std::cerr << "Error: Percentages must be non-negative" << std::endl;
-            return rolledDice; // Return an empty vector
+            SPARK_ERROR("Error: Percentages must be non-negative");
+            return rolled_dice; // Return an empty vector
         }
 
         sum += percentage;
     }
 
     if (abs(sum - 1) > 1e-4) {
-        std::cerr << "Error: The sum of percentages must be 1" << std::endl;
-        return rolledDice; // Return an empty vector
+        SPARK_ERROR("Error: The sum of percentages must be 1");
+        return rolled_dice; // Return an empty vector
     }
 
     std::random_device rd;
@@ -81,14 +79,14 @@ std::vector<u32> roll_unfair_dice(const u32 min, const unsigned int max, const s
     // Create a discrete distribution based on the given percentages
     std::discrete_distribution<u32> dist(percentages.begin(), percentages.end());
 
-    for (unsigned int i = 0; i < percentages.size(); ++i) {
-        rolledDice.push_back(dist(gen) + min); // Add min to shift the result to the desired range
+    for (u32 i = 0; i < percentages.size(); ++i) {
+        rolled_dice.push_back(dist(gen) + min); // Add min to shift the result to the desired range
     }
 
-    return rolledDice;
+    return rolled_dice;
 }
 
-void flip_coin() {
+CoinFlip flip_coin() {
     // Create a random number generator
     std::random_device rd;
     std::mt19937 gen(rd()); // Mersenne Twister engine
@@ -97,14 +95,10 @@ void flip_coin() {
     // Flip the coin
     bool result = dis(gen);
 
-    if (result) {
-        std::cout << "Heads" << std::endl;
-    } else {
-        std::cout << "Tails" << std::endl;
-    }
+    return result ? CoinFlip::HEADS : CoinFlip::TAILS;
 }
 
-void flip_unfair_coin(const double percentage) {
+CoinFlip flip_unfair_coin(const double percentage) {
     // Check if percentage is within the range [0, 1]
     if (percentage < 0 || percentage > 1) {
         std::cerr << "Error: Percentage must be between 0 and 1" << std::endl;
@@ -118,9 +112,9 @@ void flip_unfair_coin(const double percentage) {
 
     // Flip the coin
     if (dis(gen) < percentage) {
-        std::cout << "Heads" << std::endl;
+        return CoinFlip::HEADS;
     } else {
-        std::cout << "Tails" << std::endl;
+        return CoinFlip::TAILS;
     }
 }
 } // namespace math
